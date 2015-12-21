691b92c37247ac8d819369a984ba0caee0466053
realazthat <realazthat@gmail.com>
2015-12-21 15:03:46 -0500

    Fixed some errors in the dependency build scripts.

--- helpful-scripts/download-and-build-libs-with-no-pkg.sh
+++ helpful-scripts/download-and-build-libs-with-no-pkg.sh
@@ -16,15 +16,15 @@ PROJECT_PATH=$PWD
 #############################################################################
 cd $PROJECT_PATH
 cd libs
-mkdir -p cNBT
+mkdir -p cNBT && cd cNBT
 
+rm -rf ./cNBT/
 #retreive the library
 git clone https://github.com/FliPPeh/cNBT.git
 cd cNBT
 git checkout "5850e3e"
 
-mkdir -p build
-cd build
+mkdir -p build && cd build
 cmake ..
 cmake --build .
 
@@ -41,7 +41,9 @@ cmake --build .
 cd $PROJECT_PATH
 cd libs
 
-mkdir -p bprinter
+mkdir -p bprinter && cd bprinter
+
+rm -rf ./bprinter/
 git clone https://github.com/dattanchu/bprinter.git
 cd bprinter
 git checkout "29531dc"
@@ -59,7 +61,8 @@ cmake --build .
 cd $PROJECT_PATH
 cd libs
 
-mkdir -p cubelib
+mkdir -p cubelib && cd cubelib
+rm -rf ./cubelib/
 git clone https://github.com/realazthat/cubelib.git
 
 
@@ -72,13 +75,13 @@ git clone https://github.com/realazthat/cubelib.git
 cd $PROJECT_PATH
 cd libs
 
-mkdir -p threadpool
+mkdir -p ThreadPool && cd ThreadPool
+rm -rf ./ThreadPool/
 git clone https://github.com/progschj/ThreadPool.git
 cd ThreadPool
 git checkout "9a42ec1"
 
-mkdir -p build
-cd build
+mkdir -p build && cd build
 cmake ..
 cmake --build .
 
@@ -89,12 +92,13 @@ cmake --build .
 cd $PROJECT_PATH
 cd libs
 
-mkdir -p cppformat
+mkdir -p cppformat && cd cppformat
+rm -rf ./cppformat/
 git clone https://github.com/cppformat/cppformat.git
+cd cppformat
 git checkout "4797ca0"
 
-mkdir -p build
-cd build
+mkdir -p build && cd build
 cmake ..
 cmake --build .
 
