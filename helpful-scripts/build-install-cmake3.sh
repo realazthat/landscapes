7199caa851a58c855aed3b74155606a352f2f4cf
realazthat <realazthat@gmail.com>
2015-12-21 18:20:10 -0500

    Travis config now installs CMake 3.3.2, if not available.

---
+++ helpful-scripts/build-install-cmake3.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#####################################################################
+####Run this with sudo
+#####################################################################
+
+
+#exit on failed line
+set -exv
+
+PROJECT_PATH=$PWD
+
+cd ./libs
+mkdir -p cmake3 && cd cmake3
+
+rm -rf cmake-3.3.2.tar.gz
+rm -rf ./cmake-3.3.2/
+wget https://cmake.org/files/v3.3/cmake-3.3.2.tar.gz
+
+tar xf cmake-3.3.2.tar.gz
+
+cd cmake-3.3.2
+./bootstrap
+make
+make install
+
+if [`cmake --version | grep 3.3.2` -eq "cmake version 3.3.2"]
+then
+    echo "CMake is the proper version"
+else
+    echo "Invalid cmake version"
+    exit -1
+fi
---
+++ helpful-scripts/download-install-cmake3.sh
@@ -0,0 +1,39 @@
+
+#!/bin/sh
+
+#####################################################################
+####Run this with sudo
+#####################################################################
+
+
+#exit on failed line
+set -exv
+
+PROJECT_PATH=$PWD
+
+if [`cmake --version | grep 3.3.2` -eq "cmake version 3.3.2"]
+then
+    echo "CMake is the proper version"
+    exit 0
+else
+    echo "Attempting to download and install CMake 3.3.2"
+fi
+
+cd ./libs
+mkdir -p cmake3 && cd cmake3
+
+rm -rf cmake-3.2.2-Linux-x86_64.tar.gz
+rm -rf ./cmake-3.2.2-Linux-x86_64/
+wget wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
+
+tar xf cmake-3.2.2-Linux-x86_64.tar.gz
+
+sudo cp -r cmake-3.2.2-Linux-x86_64/* /usr
+
+if [`cmake --version | grep 3.3.2` -eq "cmake version 3.3.2"]
+then
+    echo "CMake is the proper version"
+else
+    echo "Invalid cmake version"
+    exit -1
+fi
