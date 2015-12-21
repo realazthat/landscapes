93395554078c43ba62d45d707a9fbb0363b2c6c5
realazthat <realazthat@gmail.com>
2015-12-21 18:39:36 -0500

    Fixed typo in cmake3 install script.

--- helpful-scripts/download-install-cmake3.sh
+++ helpful-scripts/download-install-cmake3.sh
@@ -24,7 +24,7 @@ mkdir -p cmake3 && cd cmake3
 
 rm -rf cmake-3.2.2-Linux-x86_64.tar.gz
 rm -rf ./cmake-3.2.2-Linux-x86_64/
-wget wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
+wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
 
 tar xf cmake-3.2.2-Linux-x86_64.tar.gz
 
