609ecb3cd4e4f9461bc946a2805a0f18cc58f7f9
realazthat <realazthat@gmail.com>
2015-12-22 12:29:21 -0500

    Added script for building googletest (gtest).

---
+++ scripts/download-and-build-googletest.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#####################################################################
+####Run this from the project root directory
+####This script assumes that these libraries are already there.
+#####################################################################
+
+
+#exit on failed line
+set -exv
+
+PROJECT_PATH=$PWD
+
+
+
+
+
+
+#############################################################################
+## get/build googletest
+#############################################################################
+
+#CMAKE_GENERATOR_FLAG='-GMSYS Makefiles'
+#CMAKE_GTEST_FLAG='-Dgtest_disable_pthreads=1'
+ 
+
+cd $PROJECT_PATH
+cd libs
+
+mkdir -p googletest && cd googletest
+
+rm -rf ./googletest/
+git clone https://github.com/google/googletest.git
+cd googletest
+git checkout "ddb8012"
+cd googletest
+
+mkdir -p build
+cd build
+
+if [ $(echo "$MSYSTEM" | grep -io MINGW) = "MINGW" ]; then
+    cmake -G"$CMAKE_GENERATOR" .. -Dgtest_disable_pthreads=1
+else
+    cmake -G"$CMAKE_GENERATOR" ..
+fi
+cmake --build .
+
