7802d553a9cd6218390d4b88c67c48e972a2b4b2
realazthat <realazthat@gmail.com>
2015-12-22 13:46:06 -0500

    No longer calling scripts/install-deps-via-apt.sh, travis config handles apt packages by itself. Now cloning and building glm for travis config.

---
+++ scripts/download-and-build-glm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#####################################################################
+####Run this from the project root directory
+####This script assumes that these libraries are already there.
+#####################################################################
+
+
+#exit on failed line
+set -exv
+
+PROJECT_PATH=$PWD
+
+
+
+
+
+
+#############################################################################
+## get/build glm
+#############################################################################
+
+#CMAKE_GENERATOR_FLAG='-GMSYS Makefiles'
+#CMAKE_GTEST_FLAG='-Dgtest_disable_pthreads=1'
+ 
+
+cd $PROJECT_PATH
+cd libs
+
+mkdir -p glm && cd glm
+
+rm -rf ./glm/
+git clone https://github.com/g-truc/glm.git
+cd glm
+git checkout "78f686b"
+
+
+
+
