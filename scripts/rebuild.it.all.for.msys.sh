0d56d67beb5641b913100d86601fce8a5a63fb1c
realazthat <realazthat@gmail.com>
2016-01-05 17:27:06 -0500

    Updated rebuild.it.all.for.msys.sh

--- scripts/rebuild.it.all.for.msys.sh
+++ scripts/rebuild.it.all.for.msys.sh
@@ -8,20 +8,24 @@
 
 
 
+CMAKE_DEPS_BUILD_TYPE="Debug"
+CMAKE_BUILD_TYPE="Debug"
 
+CMAKE_GENERATOR="MSYS Makefiles" CMAKE_BUILD_TYPE="$CMAKE_DEPS_BUILD_TYPE" bash ./scripts/download-and-build-googletest.sh
 
-CMAKE_GENERATOR="MSYS Makefiles" bash ./scripts/download-and-build-googletest.sh
+CMAKE_GENERATOR="MSYS Makefiles" CMAKE_BUILD_TYPE="$CMAKE_DEPS_BUILD_TYPE" bash ./scripts/download-and-build-libs-with-no-pkg.sh
 
-CMAKE_GENERATOR="MSYS Makefiles" bash ./scripts/download-and-build-glm.sh
-
-CMAKE_GENERATOR="MSYS Makefiles" bash ./scripts/download-and-build-libs-with-no-pkg.sh
-
-CMAKE_GENERATOR="MSYS Makefiles" bash ./scripts/download-unittest-data.sh
+#CMAKE_GENERATOR="MSYS Makefiles" bash ./scripts/download-unittest-data.sh
 
 rm -rf ./build
 mkdir -p build && cd build
 
 cmake -G"MSYS Makefiles" ..
+cmake . -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE"
 cmake --build .
 
 ./unittests
+
+
+
+
