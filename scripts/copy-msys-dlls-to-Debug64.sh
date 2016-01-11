0454f88df36f3b14df393fda491e72759120c056
realazthat <realazthat@gmail.com>
2016-01-11 18:26:42 -0500

    Updated copy-msys-dlls-to-Debug64.sh

--- scripts/copy-msys-dlls-to-Debug64.sh
+++ scripts/copy-msys-dlls-to-Debug64.sh
@@ -1,8 +1,31 @@
 
 
 
-cp /mingw64/bin/zlib1.dll ./Debug64/.
-cp /mingw64/bin/libwinpthread-1.dll ./Debug64/.
-cp /mingw64/bin/libgcc_s_seh-1.dll ./Debug64/.
-cp /mingw64/bin/libstdc++-6.dll ./Debug64/.
+cp -f /mingw64/bin/zlib1.dll ./Debug64/.
+cp -f /mingw64/bin/libwinpthread-1.dll ./Debug64/.
+cp -f /mingw64/bin/libgcc_s_seh-1.dll ./Debug64/.
+cp -f /mingw64/bin/libstdc++-6.dll ./Debug64/.
+cp -f /mingw64/bin/libstdc++-6.dll ./Debug64/.
+cp -f /mingw64/bin/libminizip-1.dll ./Debug64/.
+cp -f /mingw64/bin/libbz2-1.dll ./Debug64/.
+cp -f /mingw64/bin/glew32.dll ./Debug64/.
+
+
+cp -f ./libs/cegui/cegui-dependencies/build/dependencies/bin/* ./Debug64/.
+cp -f ./libs/cegui/cegui-source/build/bin/* ./Debug64/.
+cp -f ./libs/freeglut/FreeGLUT/build/bin/* ./Debug64/.
+cp -f ./libs/glfw3/glfw/build/src/*.* ./Debug64/.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
