8a169ecdf13bd4c24d479d6e635a2fb4d7800bc9
realazthat <realazthat@gmail.com>
2016-01-12 20:39:12 -0500

    Added svo_slice_inequality() routine, to compare slices

---
+++ include/landscapes/svo_slice.comparison.hpp
@@ -0,0 +1,119 @@
+#ifndef LANDSCAPES_SVO_TREE_COMPARISONS_HPP
+#define LANDSCAPES_SVO_TREE_COMPARISONS_HPP 1
+
+#include <string>
+#include <vector>
+#include <cassert>
+
+#include "landscapes/svo_tree.fwd.hpp"
+
+namespace svo{
+    
+    
+    
+    struct svo_slice_inequality_t{
+        svo_slice_inequality_t(
+                const svo_slice_t* slice0=nullptr, const svo_slice_t* slice1=nullptr
+              , const std::string& message="")
+            : slice0(slice0), slice1(slice1)
+            , message(message)
+        {
+            // message is empty => slice0 is null
+            assert( (message.size() != 0 || slice0 == nullptr) && "message is empty => slice0 is null fails");
+            // message is empty => slice1 is null
+            assert( (message.size() != 0 || slice1 == nullptr) && "message is empty => slice1 is null fails");
+            
+            
+            // slice0 is null => message is empty
+            assert( (slice0 != nullptr || message.size() == 0) && "slice0 is null => message is empty fails");
+            // slice1 is null => message is empty
+            assert( (slice1 != nullptr || message.size() == 0) && "slice1 is null => message is empty fails");
+        }
+        
+        operator bool() const{
+            return message.size() > 0;
+        }
+        
+        const svo_slice_t* slice0;
+        const svo_slice_t* slice1;
+        std::string message;
+    };
+    
+    
+    struct svo_slice_inequalities_t{
+        svo_slice_inequalities_t(const svo_slice_t* slice0, const svo_slice_t* slice1)
+            : slice0(slice0), slice1(slice1)
+        {
+            assert(slice0 != nullptr);
+            assert(slice1 != nullptr);
+        }
+            
+        operator bool() const{
+            return messages.size() > 0;
+        }
+        
+        svo_slice_inequalities_t& operator+=(const svo_slice_inequality_t& slice_inequality)
+        {
+            assert(slice_inequality.slice0 == slice0);
+            assert(slice_inequality.slice1 == slice1);
+            
+            if (slice_inequality)
+                messages.push_back(slice_inequality.message);
+            
+            return *this;
+        }
+        svo_slice_inequalities_t& operator+=(const svo_slice_inequalities_t& slice_inequalities)
+        {
+            assert(slice_inequalities.slice0 == slice0);
+            assert(slice_inequalities.slice1 == slice1);
+            
+            for (const auto& message : slice_inequalities.messages)
+                messages.push_back(message);
+            
+            return *this;
+        }
+        
+        std::vector<std::string> messages;
+        const svo_slice_t* slice0;
+        const svo_slice_t* slice1;
+    };
+
+    namespace slice_cmp_t{
+        enum enum_t{
+            
+              none =                    0
+            , level =                   1 << 1
+            , side =                    1 << 2
+            , parent_vcurve_begin =     1 << 3
+            , pos_data =                1 << 5
+            , channel_data =            1 << 6
+            , child_count =             1 << 7
+            , child_props =             1 << 7
+            , has_parent =              1 << 8
+            , parent_props =            1 << 9
+            , all_data =                pos_data | channel_data
+            , all =                     level | side | parent_vcurve_begin | pos_data | channel_data
+                                          | child_count| child_props | has_parent
+            , default_check = all
+        };
+    } // namespace svo_slice_cmp_type_t
+    
+    svo_slice_inequalities_t svo_slice_inequality(
+          const svo_slice_t* slice0
+        , const svo_slice_t* slice1
+        , slice_cmp_t::enum_t svo_slice_cmp_type = slice_cmp_t::default_check
+        , int recurse_down = 0
+        , int recurse_up = 0);
+
+
+
+
+
+
+
+
+
+} // namespace svo
+
+
+#endif
---
+++ src/landscapes/svo_slice.comparison.cpp
@@ -0,0 +1,173 @@
+
+
+#include "landscapes/svo_slice.comparison.hpp"
+#include "landscapes/svo_tree.hpp"
+
+#include "format.h"
+
+
+namespace svo{
+    
+    
+    
+    static svo_slice_inequalities_t svo_slice_inequality_internal(
+          const svo_slice_t* slice0, const std::string& name0
+        , const svo_slice_t* slice1, const std::string& name1
+        , slice_cmp_t::enum_t svo_slice_cmp_type
+        , int recurse_down
+        , int recurse_up)
+    {
+        assert(slice0);
+        assert(slice1);
+        
+        
+        svo_slice_inequalities_t results(slice0, slice1);
+        
+        if (    (svo_slice_cmp_type & slice_cmp_t::enum_t::level)
+                && slice0->level != slice1->level)
+            results += svo_slice_inequality_t(slice0, slice1,
+                            fmt::format("{0}->level != {1}->level"
+                                        ", {0}->level: {2}, {1}->level: {3}"
+                                        , name0, name1
+                                        , slice0->level, slice1->level));
+        
+        if (   (svo_slice_cmp_type & slice_cmp_t::enum_t::side)
+               && slice0->side != slice1->side)
+            results += svo_slice_inequality_t(slice0, slice1,
+                            fmt::format("{0}->side != {1}->side"
+                                        ", {0}->side: {2}, {1}->side: {3}"
+                                        , name0, name1
+                                        , slice0->side, slice1->side));
+        
+        if ( (svo_slice_cmp_type & slice_cmp_t::enum_t::parent_vcurve_begin) 
+             && slice0->parent_vcurve_begin != slice1->parent_vcurve_begin)
+            results += svo_slice_inequality_t(slice0, slice1,
+                            fmt::format("{0}->parent_vcurve_begin != {1}->parent_vcurve_begin"
+                                        ", {0}->parent_vcurve_begin: {2}, {1}->parent_vcurve_begin: {3}"
+                                        , name0, name1
+                                        , slice0->parent_vcurve_begin, slice1->parent_vcurve_begin));
+        
+        auto slice0_has_parent = slice0->parent_slice != nullptr;
+        auto slice1_has_parent = slice1->parent_slice != nullptr;
+        bool should_care_about_parent =  (svo_slice_cmp_type & slice_cmp_t::enum_t::has_parent)
+                                    ||  (svo_slice_cmp_type & slice_cmp_t::enum_t::parent_props); 
+        if ( should_care_about_parent && (slice0_has_parent != slice1_has_parent))
+            results += svo_slice_inequality_t(slice0, slice1,
+                            fmt::format("{0}->parent is {2} and {1}->parent is {3}"
+                                        , name0, name1
+                                        , (slice0_has_parent ? "is null" : "is not null")
+                                        , (slice1_has_parent ? "is null" : "is not null")));
+        
+        if ((svo_slice_cmp_type & slice_cmp_t::enum_t::parent_props) && slice0_has_parent && slice1_has_parent)
+        {
+            assert(false && "TODO");
+        }
+        
+        
+        assert(slice0->children);
+        assert(slice1->children);
+        
+        const auto& children0 = *slice0->children;
+        const auto& children1 = *slice1->children;
+        
+        
+        bool should_care_about_child_count = (svo_slice_cmp_type & slice_cmp_t::enum_t::child_count)  || (svo_slice_cmp_type & slice_cmp_t::enum_t::child_props);
+        if (should_care_about_child_count && children0.size() != children1.size())
+            results += svo_slice_inequality_t(slice0, slice1,
+                            fmt::format("{0}->children->size() != {1}->children->size()"
+                                        ", {0}->children->size(): {2}, {1}->children->size(): {3}"
+                                        , name0, name1
+                                        , children0.size(), children1.size()));
+        
+        
+        if ((svo_slice_cmp_type & slice_cmp_t::enum_t::child_props) && children0.size() == children1.size())
+        {
+            std::size_t child_count = slice0->children->size();
+            
+            for (std::size_t child_index = 0; child_index < child_count; ++child_index)
+            {
+                const auto* child0 = children0[child_index];
+                const auto* child1 = children1[child_index];
+                assert(child0);
+                assert(child1);
+                
+                std::string child0_name = fmt::format("{0}->children[{1}]", name0, child_index);
+                std::string child1_name = fmt::format("{0}->children[{1}]", name1, child_index);
+                
+                slice_cmp_t::enum_t child_cmp_mask = slice_cmp_t::enum_t(
+                                          slice_cmp_t::enum_t::level | slice_cmp_t::enum_t::side
+                                        | slice_cmp_t::enum_t::parent_vcurve_begin
+                                        | slice_cmp_t::enum_t::has_parent | slice_cmp_t::enum_t::child_count);
+                child_cmp_mask = slice_cmp_t::enum_t(child_cmp_mask & svo_slice_cmp_type);
+                results += svo_slice_inequality_internal(slice0, child0_name, slice1, child1_name
+                                    , child_cmp_mask, 0 /*recurse_down*/, 0 /*recurse_up*/);
+                
+                
+                if (svo_slice_cmp_type & slice_cmp_t::enum_t::has_parent)
+                {
+                    bool child0_points_back_to_parent = child0->parent_slice == slice0;
+                    bool child1_points_back_to_parent = child1->parent_slice == slice1;
+                    
+                    if (child0_points_back_to_parent != child1_points_back_to_parent)
+                        results += svo_slice_inequality_t(slice0, slice1,
+                            fmt::format("{0} {2}, but {1} {3}"
+                                        , child0_name, child1_name
+                                        , (child0_points_back_to_parent ? "points back to parent" : "does not point back to parent")
+                                        , (child1_points_back_to_parent ? "points back to parent" : "does not point back to parent")
+                                        ));
+                        
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        
+        if ((recurse_down > 0) && children0.size() == children1.size())
+        {
+            std::size_t child_count = slice0->children->size();
+            
+            for (std::size_t child_index = 0; child_index < child_count; ++child_index)
+            {
+            
+                std::string child0_name = fmt::format("{0}->children[{1}]", name0, child_index);
+                std::string child1_name = fmt::format("{0}->children[{1}]", name1, child_index);
+                results += svo_slice_inequality_internal(slice0, child0_name, slice1, child1_name
+                                    , svo_slice_cmp_type, recurse_down-1 /*recurse_down*/, 0 /*recurse_up*/);
+                                
+            }
+        }
+        
+        if ((recurse_up > 0) && (slice0_has_parent && slice1_has_parent))
+        {
+            
+            std::string child0_name = fmt::format("{0}->parent", name0);
+            std::string child1_name = fmt::format("{0}->parent", name1);
+            results += svo_slice_inequality_internal(slice0, child0_name, slice1, child1_name
+                                , svo_slice_cmp_type, 1 /*recurse_down*/, recurse_up - 1/*recurse_up*/);
+            
+        }
+        return results;
+    }
+
+
+
+
+
+    svo_slice_inequalities_t svo_slice_inequality(
+          const svo_slice_t* slice0
+        , const svo_slice_t* slice1
+        , slice_cmp_t::enum_t svo_slice_cmp_type
+        , int recurse_down
+        , int recurse_up)
+    {
+        return svo_slice_inequality_internal(slice0, "slice0", slice1, "slice1", svo_slice_cmp_type, recurse_down, recurse_up);
+    }
+
+
+
+
+} // namespace
+
