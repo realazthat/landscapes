9fc5a8a6290cad4e4ba882ea7a139657dc9b0968
realazthat <realazthat@gmail.com>
2016-01-14 13:43:18 -0500

    Added tests for svo_buffer_t enums

--- src/unittests/constants.cpp
+++ src/unittests/constants.cpp
@@ -1,6 +1,7 @@
 
 #include "landscapes/svo_inttypes.h"
 #include "landscapes/svo_curves.h"
+#include "landscapes/svo_buffer.fwd.hpp"
 #include "gtest/gtest.h"
 #include <fstream>
 #include <bitset>
@@ -58,5 +59,66 @@ TEST_F(ConstantsTest,constants){
     // fit in to 1/3 of the value-space of a vcurve_t.
     EXPECT_EQ(SVO_VSIDE_LIMIT*SVO_VSIDE_LIMIT*SVO_VSIDE_LIMIT, SVO_VCURVE_LIMIT);
     EXPECT_LE(SVO_VSIDE_BITS, 8*sizeof(vcurve_t)/3);
+    
+    
+    /// svo::svo_semantic_t enum sanity
+    {
+        auto all_semantics_set1 = std::set<svo::svo_semantic_t>(svo::all_semantics.begin(), svo::all_semantics.end());
+        EXPECT_EQ(svo::all_semantics_set, all_semantics_set1);
+        
+        for (svo::svo_semantic_t semantic : svo::all_semantics_set)
+        {
+            // the switch should generate a warning if we are missing a semantic
+            switch (semantic)
+            {
+                case(svo::svo_semantic_t::NONE):
+                case(svo::svo_semantic_t::COLOR):
+                case(svo::svo_semantic_t::NORMAL):
+                    continue;
+            }
+        }
+        
+        
+        EXPECT_TRUE(svo::all_semantics_set.count(svo::svo_semantic_t::NONE));
+        EXPECT_TRUE(svo::all_semantics_set.count(svo::svo_semantic_t::COLOR));
+        EXPECT_TRUE(svo::all_semantics_set.count(svo::svo_semantic_t::NORMAL));
+    }
+    
+    /// svo::svo_data_type_t enum sanity
+    {
+        auto all_buffer_data_types_set1 = std::set<svo::svo_data_type_t>(svo::all_buffer_data_types.begin(), svo::all_buffer_data_types.end());
+        EXPECT_EQ(svo::all_buffer_data_types_set, all_buffer_data_types_set1);
+        
+        for (svo::svo_data_type_t data_type : svo::all_buffer_data_types_set)
+        {
+            // the switch should generate a warning if we are missing a semantic
+            switch (data_type)
+            {
+                case(svo::svo_data_type_t::BYTE):
+                case(svo::svo_data_type_t::UNSIGNED_BYTE):
+                case(svo::svo_data_type_t::SHORT):
+                case(svo::svo_data_type_t::UNSIGNED_SHORT):
+                case(svo::svo_data_type_t::INT):
+                case(svo::svo_data_type_t::UNSIGNED_INT):
+                case(svo::svo_data_type_t::LONG):
+                case(svo::svo_data_type_t::UNSIGNED_LONG):
+                case(svo::svo_data_type_t::FLOAT):
+                case(svo::svo_data_type_t::DOUBLE):
+                    continue;
+            }
+        }
+        
+        
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::BYTE));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::UNSIGNED_BYTE));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::SHORT));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::UNSIGNED_SHORT));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::INT));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::UNSIGNED_INT));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::LONG));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::UNSIGNED_LONG));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::FLOAT));
+        EXPECT_TRUE(svo::all_buffer_data_types_set.count(svo::svo_data_type_t::DOUBLE));
+    }
 }
 
